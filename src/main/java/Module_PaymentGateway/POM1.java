5388a3ed4f45dbcab9e69f7a1d83db7a314eab06
NaruleNikhil <97401494+NaruleNikhil@users.noreply.github.com>
2022-08-26 15:46:51 +0530

    updated stag changes

---
+++ src/main/java/Module_PaymentGateway/POM1.java
@@ -0,0 +1,11 @@
+package Module_PaymentGateway;
+
+public class POM1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+		System.out.println("Vlidation of Payment Gateway Module");
+		}
+
+}
